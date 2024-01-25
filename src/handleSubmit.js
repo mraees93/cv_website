d080ac84686a06015dbc48255deca27b9b99646e
mraees93 <53144654+mraees93@users.noreply.github.com>
2024-01-25 17:52:05 +0200

    debug

--- src/handleSubmit.js
+++ src/handleSubmit.js
@@ -1,34 +1,23 @@
-const { MongoClient } = require("mongodb");
-const mongoClient = new MongoClient(process.env.MONGODB_URI);
-
-const clientPromise = mongoClient.connect();
-
 const handleSubmit = async (event) => {
   event.preventDefault();
   console.log("start");
   const myForm = event.target;
   const formData = new FormData(myForm);
   const urlFormParam = paramCreator(formData);
-  console.log(formData);
-
   await fetch("/contact.html", {
     method: "POST",
     headers: { "Content-Type": "application/x-www-form-urlencoded" },
     body: formData.toString(),
   })
     .then(async () => {
-      console.log("inside then", urlFormParam);
-      //   await fetch(`/.netlify/functions/dbConnections?${urlFormParam}`).then(
-      //     (response) => {
-      //       console.log(response);
-      //       if (response.status == 200) window.alert("Sent");
-      //       response.json();
-      //     }
-      //   );
-      const database = (await clientPromise).db(process.env.MONGODB_DB);
-      const collection = database.collection(process.env.MONGODB_COLLECTION);
-
-      collection.insertOne(urlFormParam);
+      console.log("inside then");
+      await fetch(`/.netlify/functions/dbConnections?${urlFormParam}`).then(
+        (response) => {
+          console.log(response);
+          if (response.status == 200) window.alert("Sent");
+          response.json();
+        }
+      );
       console.log("after then");
     })
     .catch((error) => window.alert(error, error.message));
@@ -37,14 +26,13 @@ const handleSubmit = async (event) => {
 document.querySelector("form").addEventListener("submit", handleSubmit);
 
 const paramCreator = (data) => {
-  let paramStr = {};
+  let paramStr = "";
   const keyPos = Object.keys(data);
   let count = 0;
   for (const [key, value] of data) {
-    // if (count < 2) paramStr += `${key}=${value}&`;
-    // else paramStr += `${key}=${value}`;
-    // count++;
-    paramStr[key] = value;
+    if (count < 2) paramStr += `${key}=${value}&`;
+    else paramStr += `${key}=${value}`;
+    count++;
   }
   return paramStr;
 };
