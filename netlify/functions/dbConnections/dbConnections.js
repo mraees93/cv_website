e986e95ffaf5f12571579d6fa4218638437073da
mraees93 <53144654+mraees93@users.noreply.github.com>
2024-01-25 22:59:10 +0200

    debug

--- netlify/functions/dbConnections/dbConnections.js
+++ netlify/functions/dbConnections/dbConnections.js
@@ -4,13 +4,13 @@ const mongoClient = new MongoClient(process.env.MONGODB_URI);
 const clientPromise = mongoClient.connect();
 
 const handler = async (event, context) => {
-  context.callbackWaitsForEmptyEventLoop = false;
   try {
     const params = event.queryStringParameters;
     const database = (await clientPromise).db(process.env.MONGODB_DB);
     const collection = database.collection(process.env.MONGODB_COLLECTION);
 
     collection.insertOne(params);
+    context.callbackWaitsForEmptyEventLoop = false;
 
     return {
       statusCode: 200,
